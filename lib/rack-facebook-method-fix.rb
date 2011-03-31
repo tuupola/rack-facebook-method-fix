04bf97f6ac052df465199e09b1ce52c183e1c2be
Mika Tuupola <tuupola@appelsiini.net>
2011-03-31 13:14:03 +0300

    Reorganize modules.

---
+++ lib/rack-facebook-method-fix.rb
@@ -0,0 +1,1 @@
+require "rack/facebook/method-fix"
--- lib/rack/facebook/method-fix.rb
+++ lib/rack/facebook/method-fix.rb
@@ -1,18 +1,19 @@
 module Rack
-  class FacebookMethodFix
+  module Facebook
+    class MethodFix
 
-    def initialize(app)
-      @app = app
-    end
-
-    def call(env)
-      if env["REQUEST_METHOD"] == "POST"
-        req = Request.new(env)
-        env["REQUEST_METHOD"] = "GET" if req.params["signed_request"]
+      def initialize(app)
+        @app = app
       end
-      @app.call(env)
-    end
+
+      def call(env)
+        if env["REQUEST_METHOD"] == "POST"
+          req = Request.new(env)
+          env["REQUEST_METHOD"] = "GET" if req.params["signed_request"]
+        end
+        @app.call(env)
+      end
     
+    end
   end
-  
 end
