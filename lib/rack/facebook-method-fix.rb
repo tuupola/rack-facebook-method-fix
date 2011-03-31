1c54993aa6812ce994f8ec6a87d6548f80431bc5
Mika Tuupola <tuupola@appelsiini.net>
2011-03-31 12:46:15 +0300

    Remove debug code.

--- lib/rack/facebook-method-fix.rb
+++ lib/rack/facebook-method-fix.rb
@@ -6,17 +6,13 @@ module Rack
     end
 
     def call(env)
-      pp "BEFORE"
-      pp env["REQUEST_METHOD"]
       if env["REQUEST_METHOD"] == "POST"
         req = Request.new(env)
         env["REQUEST_METHOD"] = "GET" if req.params["signed_request"]
       end
-      pp "AFTER"
-      pp env["REQUEST_METHOD"]
-
       @app.call(env)
     end
+    
   end
   
 end
