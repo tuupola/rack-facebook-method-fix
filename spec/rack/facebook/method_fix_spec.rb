8d1e767d5a52599eca6d8afcc09f7fc097455703
Brian Butz & Kevin Fitzpatrick <pair+bbutz+kevin@pivotallabs.com>
2011-06-24 16:28:46 -0700

    Can now exclude requests by passing an :exclude => proc option.

--- spec/rack/facebook/method_fix_spec.rb
+++ spec/rack/facebook/method_fix_spec.rb
@@ -1,6 +1,5 @@
 require File.expand_path('spec_helper', File.join(File.dirname(__FILE__), '../../'))
 
-
 class MockRackWrapper
   def initialize(response)
     @response = response
@@ -38,6 +37,34 @@ describe Rack::Facebook::MethodFix do
         @request.post("/", {:params => {"signed_request" => 'nothing'}})
 
         @rack_mock.env["REQUEST_METHOD"].should == "GET"
+      end
+    end
+  end
+
+  context 'when the middleware is passed an exclusion proc' do
+    before do
+      simple_response = [200, {"Content-type" => "test/plain", "Content-length" => "5"}, ["foo"]]
+      @mock_rack_app = MockRackWrapper.new(simple_response)
+      exclusion_proc = proc { |env| env['PATH_INFO'].match(/^\/admin/) }
+      facebook_method_fix_app = Rack::Facebook::MethodFix.new(@mock_rack_app, :exclude => exclusion_proc)
+      @request = Rack::MockRequest.new(facebook_method_fix_app)
+    end
+
+    it "does not change requests that are not from facebook" do
+      @request.post('/', {})
+      @mock_rack_app.env["REQUEST_METHOD"].should == "POST"
+    end
+
+    context "requests from facebook " do
+      let(:params) { {:params => {"signed_request" => 'nothing'}} }
+      it "changes POSTs to GETs the exclusion proc returns false" do
+        @request.post('/foo', params)
+        @mock_rack_app.env["REQUEST_METHOD"].should == "GET"
+      end
+
+      it "does not change POSTs when the exclusion proc returns true" do
+        @request.post('/admin/foo', params)
+        @mock_rack_app.env["REQUEST_METHOD"].should == "POST"
       end
     end
   end
